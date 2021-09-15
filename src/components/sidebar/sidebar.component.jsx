633dc96c3d8afce9cc0a08f15685e41acdd816f1
PatriciaJ <patriciagj@live.com>
2021-09-15 14:28:45 +0100

    added filter for the favourites countries

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { Search } from '../search/search.component';
 
 import './sidebar.component.styles.css';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 export const SideBar = () => {
   const favourites = useSelector(state => state.favourites);
+
+  const [filteredFavourite, setFavourite] = useState();
+
+  const filterFavourite = event => {
+    console.log(event.target.value);
+    setFavourite(event.target.value);
+  };
+
   return (
     <div className='sidebar'>
-      {favourites.map((country, index) => {
-        return (
-          <div className='country' key={index}>
-            {country}
-          </div>
-        );
-      })}
+      <Search onChange={filterFavourite} />
+      {favourites
+        .filter(country =>
+          country === filteredFavourite ? filteredFavourite : !filteredFavourite
+        )
+        .map((country, index) => {
+          return (
+            <div className='country' key={index}>
+              {country}
+            </div>
+          );
+        })}
     </div>
   );
 };
