ea6d791676bbef35577b949c30b88fabf9f94532
PatriciaJ <patriciagj@live.com>
2021-09-15 18:16:19 +0100

    implemented Material-UI on Select Region component

--- src/components/select-region/select-region.component.jsx
+++ src/components/select-region/select-region.component.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 
-import './select-region.component.styles.css';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import useStyles from './select-region.component.styles';
 
 export const SelectRegion = props => {
+  const classes = useStyles();
+  const [select, setSelect] = React.useState('');
+
+  const handleChange = event => {
+    setSelect(event.target.value);
+    props.onChange(event);
+  };
+
   return (
-    <section className='select'>
-      <select
-        name='select'
-        id='select'
-        className='select__country'
-        onChange={event => props.onChange(event)}
+    <div>
+      <FormControl
+        variant='outlined'
+        size='small'
+        className={classes.formControl}
       >
-        <option value='Select Region'>Select Region</option>
-        <option value='Africa'>Africa</option>
-        <option value='Americas'>America</option>
-        <option value='Asia'>Asia</option>
-        <option value='Europe'>Europe</option>
-        <option value='Oceania'>Oceania</option>
-      </select>
-    </section>
+        <InputLabel id='select-outlined-label'>Region</InputLabel>
+        <Select
+          labelId='select-outlined-label'
+          id='select-outlined'
+          value={select}
+          label='Select'
+          onChange={event => handleChange(event)}
+        >
+          <MenuItem value={'Select Region'}>Select Region</MenuItem>
+          <MenuItem value={'Africa'}>Africa</MenuItem>
+          <MenuItem value={'Americas'}>Americas</MenuItem>
+          <MenuItem value={'Asia'}>Asia</MenuItem>
+          <MenuItem value={'Europe'}>Europe</MenuItem>
+          <MenuItem value={'Oceania'}>Oceania</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
   );
 };
---
+++ src/components/select-region/select-region.component.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 150,
+  },
+}));
+
+export default useStyles;
