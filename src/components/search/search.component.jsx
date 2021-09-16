2036fbe6a788d7ffd7b1a3435c0dfe1f36644488
PatriciaJ <patriciagj@live.com>
2021-09-16 14:11:28 +0100

    implemented Material-UI on Search component

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 
-import './search.component.styles.css';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from '../search/search.component.styles';
 
 export const Search = props => {
+  const classes = useStyles();
+
   return (
-    <form>
-      <input
-        type='search'
-        className='search'
-        placeholder='Search country'
-        onChange={event => props.onChange(event)}
-      ></input>
-    </form>
+    <div>
+      <FormControl className={classes.margin} size='small'>
+        <Input
+          id='input-with-icon-adornment'
+          type='search'
+          className='search'
+          placeholder='Search country'
+          onChange={event => props.onChange(event)}
+          startAdornment={
+            <InputAdornment position='start'>
+              <SearchIcon />
+            </InputAdornment>
+          }
+        ></Input>
+      </FormControl>
+    </div>
   );
 };
---
+++ src/components/search/search.component.styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default useStyles;
