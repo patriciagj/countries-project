dda687b65b7c6186f3404e91be3739acc5d16f67
PatriciaJ <patriciagj@live.com>
2021-09-16 14:30:56 +0100

    implemented Material-UI on Sidebar component

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -3,9 +3,15 @@ import { useSelector } from 'react-redux';
 
 import { Search } from '../search/search.component';
 
-import './sidebar.component.styles.css';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+
+import useStyles from './sidebar.component.styles';
 
 export const SideBar = () => {
+  const classes = useStyles();
   const favourites = useSelector(state => state.favourites);
 
   const [filteredFavourite, setFavourite] = useState();
@@ -16,19 +22,35 @@ export const SideBar = () => {
   };
 
   return (
-    <div className='sidebar'>
-      <Search onChange={filterFavourite} />
-      {favourites
-        .filter(country =>
-          country === filteredFavourite ? filteredFavourite : !filteredFavourite
-        )
-        .map((country, index) => {
-          return (
-            <div className='country' key={index}>
-              {country}
-            </div>
-          );
-        })}
+    <div className={classes.root}>
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor='left'
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <Search onChange={filterFavourite} />
+          {favourites
+            .filter(country =>
+              country === filteredFavourite
+                ? filteredFavourite
+                : !filteredFavourite
+            )
+            .map((country, index) => {
+              return (
+                <div className='country' key={index}>
+                  {country}
+                </div>
+              );
+            })}
+        </List>
+      </Drawer>
     </div>
   );
 };
---
+++ src/components/sidebar/sidebar.component.styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 250;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+export default useStyles;
